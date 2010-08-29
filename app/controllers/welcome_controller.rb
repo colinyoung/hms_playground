f841dc04b7b3f2d6591117864311db65740f2e65
Colin Young <me@colinyoung.name>
2010-08-29 18:44:18 -0500

    added ajax to map view and removed redirect post and action from welcome controller

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,13 +4,4 @@ class WelcomeController < ApplicationController
     # normally, this would come from the User model from a previous step.
     @zipcode = params[:zipcode] || "60657"
   end
-  
-  def redirect
-    if params[:search_string].blank?
-      render :text => "blank request"
-    else
-      # redirect_to :controller => "workplaces", :action => "show_map", :id => params[:search_string] 
-      redirect_to "/map/#{params[:search_string]}"
-    end
-  end
 end
